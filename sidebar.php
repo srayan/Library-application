a46c3ee33ec4b8005ee23cff873893607e156358
Srayan Guhathakurta <srayanguhathakurta@yahoo.com>
2014-06-07 18:08:57 -0500

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,34 @@
+<aside class="related three columns" id="actionSidebar">
+      <a href="index.php"><img src="_images/book_worm.png"></a>
+
+      <br>
+
+      <div id="buttonHolder">
+        <ul type="none">
+          <a href="index.php">
+            <li><img src="_images/home.png"> &nbsp; Home</li>
+          </a>
+          <a href="search.php">
+            <li><img src="_images/search.png"> &nbsp; Search Books</li>
+          </a>
+          <a href="checkout.php">
+            <li><img src="_images/checkout.png"> &nbsp; Check Out</li>
+          </a>
+          <a href="checkin.php">
+            <li><img src="_images/checkin.png"> &nbsp; Check In</li>
+          </a>
+          <a href="add_borrower.php">
+            <li><img src="_images/add.png"> &nbsp; Add Borrower</li>
+          </a>
+          <a href="history.php">
+            <li><img src="_images/history.png"> &nbsp; History</li>
+          </a>
+          <a href="fines.php">
+            <li><img src="_images/fine.png"> &nbsp; Fines &amp; Dues</li>
+          </a>
+          <a href="docs.php">
+            <li><img src="_images/info.png"> &nbsp; About Me</li>
+          </a>
+        </ul> 
+      </div>
+</aside>
