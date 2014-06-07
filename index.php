0bbb19423704138cb65d05b86c691d8d21a4982e
Srayan Guhathakurta <srayanguhathakurta@yahoo.com>
2014-06-07 18:08:28 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Book Worm</title>
+
+  <script src="_scripts/jquery.js"></script>
+
+  <link rel="stylesheet" href="_css/foundation.css">
+  <link rel="stylesheet" href="_css/main.css">
+
+  <link rel="Shortcut icon" href="_images/book_worm.ico">
+
+  <!--<script src="_scripts/modernizr.foundation.js"></script>-->
+  <style>
+  h2 {
+	  font-size: 160%;
+  }
+  </style>
+</head>
+<body>
+
+  <!-- Main Content -->
+  <div class="row">
+    <?php include 'sidebar.php'; ?>
+
+    <article class="main nine columns">
+       
+        <div align="center"> <h1> Book Worm </h1> </div>
+      
+
+        <p>Book Worm is an interactive web service that helps you
+          <ul class="textIndent">
+            <li>Search for the books you want to read</li>
+            <li>Check Out books</li>
+            <li>Check In books</li>
+            <li>Add a new borrower</li>
+          </ul>
+        </p>
+
+        <p> <i>Borrower rules and regulations</i>
+          <ul class="textIndent">
+            <li>You need to have a borrower's card to loan books</li>
+            <li>A single borrower can loan a maximum of three books</li>
+            <li>2 week book loan period</li>
+          </ul>
+        </p>
+
+        <p><i>Go ahead and enjoy the pages </i></p>
+    </article>
+
+  </div>
+  
+
+<script src="_scripts/foundation.min.js"></script>
+  <script src="_scripts/app.js"></script>
+</body>
+</html>
